b4cc7cf232429f7cb4ed26ccd241c23f9f607553
Toni <toni.einecker@gmail.com>
2020-11-12 11:53:49 +0100

    Added DB-Access for Events and Items loading from db

--- src/pages/PageEvents.vue
+++ src/pages/PageEvents.vue
@@ -11,14 +11,15 @@
               <q-btn color="positive" class="float-right" icon="add" label="Neues Event erstellen"/>
             </div>
           </div>
-          <div class="row">
+          <div class="row"
+               v-for="event in events">
             <div class="col">
               <q-card class="my-card">
                 <q-card-section>
                   <div class="row items-center no-wrap">
                     <div class="col">
-                      <div class="text-h5">Silvester Raclette</div>
-                      <div class="text-subtitle2 q-pa-sm">erstellt von: Janis Fix</div>
+                      <div class="text-h5">{{ event.Description }}</div>
+                      <div class="text-subtitle2 q-pa-sm">erstellt von: {{ event.Creator }}</div>
                     </div>
                     <div class="col-auto">
                       <q-btn color="grey-7" round flat icon="more_vert">
@@ -45,7 +46,7 @@
 
                       <q-item-section>
                         <q-item-label>Wo</q-item-label>
-                        <q-item-label caption>Bei Janis im Keller</q-item-label>
+                        <q-item-label caption>{{ event.Location }}</q-item-label>
                       </q-item-section>
                     </q-item>
                     <q-item>
@@ -55,7 +56,7 @@
 
                       <q-item-section>
                         <q-item-label>Wann</q-item-label>
-                        <q-item-label caption>31.12.2020, ab 19 Uhr</q-item-label>
+                        <q-item-label caption>{{ event.Timestamp }}</q-item-label>
                       </q-item-section>
                     </q-item>
                   </q-list>
@@ -69,48 +70,16 @@
                   >
                     <q-card>
                       <q-list>
-                        <q-item>
+                        <q-item
+                          v-for="item in event.Items">
                           <q-item-section avatar>
                             <q-avatar size="28px">
                               <img src="https://cdn.pixabay.com/photo/2018/01/13/19/39/fashion-3080644__480.jpg">
                             </q-avatar>
                           </q-item-section>
                           <q-item-section>
-                            <q-item-label>Tomaten</q-item-label>
-                            <q-item-label caption>17 frisch gepflückt</q-item-label>
-                          </q-item-section>
-                        </q-item>
-                        <q-item>
-                          <q-item-section avatar>
-                            <q-avatar size="28px">
-                              <img src="https://cdn.pixabay.com/photo/2015/04/27/22/53/man-742766__480.jpg">
-                            </q-avatar>
-                          </q-item-section>
-                          <q-item-section>
-                            <q-item-label>Mais</q-item-label>
-                            <q-item-label caption>2 Dosen</q-item-label>
-                          </q-item-section>
-                        </q-item>
-                        <q-item>
-                          <q-item-section avatar>
-                            <q-avatar size="28px">
-                              <img src="https://cdn.pixabay.com/photo/2018/01/13/19/39/fashion-3080644__480.jpg">
-                            </q-avatar>
-                          </q-item-section>
-                          <q-item-section>
-                            <q-item-label>Äpfel</q-item-label>
-                            <q-item-label caption>Keine! Ich mein, wer macht Äpfel auf sein Raclette?</q-item-label>
-                          </q-item-section>
-                        </q-item>
-                        <q-item>
-                          <q-item-section avatar>
-                            <q-avatar size="28px">
-                              <img src="https://cdn.pixabay.com/photo/2015/01/27/09/58/human-613601__480.jpg">
-                            </q-avatar>
-                          </q-item-section>
-                          <q-item-section>
-                            <q-item-label>Pilze</q-item-label>
-                            <q-item-label caption>2 Kilo</q-item-label>
+                            <q-item-label>{{ item.Name }}</q-item-label>
+                            <q-item-label caption>{{ item.Amount }}</q-item-label>
                           </q-item-section>
                         </q-item>
                       </q-list>
@@ -122,15 +91,10 @@
                     <div class="col col-12 col-md-6 q-pa-sm">
                       <div class="text-h6 q-pa-sm">Teilnehmer:
                         <div class="q-gutter-x-sm">
-                          <q-avatar class="q-pa-sm" size="28px">
+                          <q-avatar class="q-pa-sm" size="28px"
+                                    v-for="user in event.Group">
                             <img src="https://cdn.pixabay.com/photo/2018/01/13/19/39/fashion-3080644__480.jpg">
                           </q-avatar>
-                          <q-avatar class="q-pa-sm" size="28px">
-                            <img src="https://cdn.pixabay.com/photo/2015/01/27/09/58/human-613601__480.jpg">
-                          </q-avatar>
-                          <q-avatar class="q-pa-sm" size="28px">
-                            <img src="https://cdn.pixabay.com/photo/2015/04/27/22/53/man-742766__480.jpg">
-                          </q-avatar>
                         </div>
                       </div>
                     </div>
@@ -138,149 +102,7 @@
                 </q-card-section>
               </q-card>
             </div>
-          </div>
-          <br>
-          <div class="row">
-            <div class="col">
-              <q-card class="my-card">
-                <q-card-section>
-                  <div class="row items-center no-wrap">
-                    <div class="col">
-                      <div class="text-h5">Abschieds Essen</div>
-                      <div class="text-subtitle2 q-pa-sm">erstellt von: Tobias Bühler</div>
-                    </div>
-                    <div class="col-auto">
-                      <q-btn color="grey-7" round flat icon="more_vert">
-                        <q-menu cover auto-close>
-                          <q-list>
-                            <q-item clickable>
-                              <q-item-section>Event verlassen</q-item-section>
-                            </q-item>
-                            <q-item clickable>
-                              <q-item-section>Freund einladen</q-item-section>
-                            </q-item>
-                          </q-list>
-                        </q-menu>
-                      </q-btn>
-                    </div>
-                  </div>
-                </q-card-section>
-                <q-card-section class="q-pt-none">
-                  <q-list>
-                    <q-item>
-                      <q-item-section avatar>
-                        <q-icon color="dark" name="place"/>
-                      </q-item-section>
-
-                      <q-item-section>
-                        <q-item-label>Wo</q-item-label>
-                        <q-item-label caption>In der Wohnung in Karlsruhe</q-item-label>
-                      </q-item-section>
-                    </q-item>
-                    <q-item>
-                      <q-item-section avatar>
-                        <q-icon color="dark" name="today"/>
-                      </q-item-section>
-
-                      <q-item-section>
-                        <q-item-label>Wann</q-item-label>
-                        <q-item-label caption>30.11.2020, ab 19 Uhr</q-item-label>
-                      </q-item-section>
-                    </q-item>
-                  </q-list>
-                </q-card-section>
-                <q-card-section>
-                  <q-expansion-item
-                    expand-separator
-                    icon="list"
-                    label="Zutaten"
-                    caption="noch nicht vollständig"
-                  >
-                    <q-card>
-                      <q-list>
-                        <q-item>
-                          <q-item-section avatar>
-                            <q-avatar size="28px">
-                              <img src="https://cdn.pixabay.com/photo/2017/08/30/12/45/girl-2696947__480.jpg">
-                            </q-avatar>
-                          </q-item-section>
-                          <q-item-section>
-                            <q-item-label>Tomaten</q-item-label>
-                            <q-item-label caption>17 frisch gepflückt</q-item-label>
-                          </q-item-section>
-                        </q-item>
-                        <q-item>
-                          <q-item-section avatar>
-                            <q-avatar size="28px">
-                              <img src="https://cdn.pixabay.com/photo/2017/08/30/12/45/girl-2696947__480.jpg">
-                            </q-avatar>
-                          </q-item-section>
-                          <q-item-section>
-                            <q-item-label>Mais</q-item-label>
-                            <q-item-label caption>2 Dosen</q-item-label>
-                          </q-item-section>
-                        </q-item>
-                        <q-item>
-                          <q-item-section avatar>
-                            <q-avatar size="28px">
-                              <img src="https://cdn.pixabay.com/photo/2017/08/30/12/45/girl-2696947__480.jpg">
-                            </q-avatar>
-                          </q-item-section>
-                          <q-item-section>
-                            <q-item-label>Äpfel</q-item-label>
-                            <q-item-label caption>Keine! Ich mein, wer macht Äpfel auf sein Raclette?</q-item-label>
-                          </q-item-section>
-                        </q-item>
-                        <q-item>
-                          <q-item-section avatar>
-                            <q-avatar size="28px">
-                              <img src="https://cdn.pixabay.com/photo/2015/03/03/20/42/man-657869__480.jpg">
-                            </q-avatar>
-                          </q-item-section>
-                          <q-item-section>
-                            <q-item-label>Pilze</q-item-label>
-                            <q-item-label caption>2 Kilo</q-item-label>
-                          </q-item-section>
-                        </q-item>
-                        <q-item>
-                          <q-item-section avatar>
-                            <q-avatar size="28px">
-                              <img src="https://cdn.pixabay.com/photo/2015/03/03/20/42/man-657869__480.jpg">
-                            </q-avatar>
-                          </q-item-section>
-                          <q-item-section>
-                            <q-item-label>Annanas</q-item-label>
-                            <q-item-label caption>2 Stück</q-item-label>
-                          </q-item-section>
-                        </q-item>
-                      </q-list>
-                    </q-card>
-                  </q-expansion-item>
-                </q-card-section>
-                <q-card-section>
-                  <div class="row">
-                    <div class="col col-12 col-md-6 q-pa-sm">
-                      <div class="text-h6 q-pa-sm">Teilnehmer:
-                        <div class="q-gutter-x-sm">
-                          <q-avatar class="q-pa-sm" size="28px">
-                            <img src="https://cdn.pixabay.com/photo/2017/08/30/12/45/girl-2696947__480.jpg">
-                          </q-avatar>
-                          <q-avatar class="q-pa-sm" size="28px">
-                            <img src="https://cdn.pixabay.com/photo/2015/03/03/20/42/man-657869__480.jpg">
-                          </q-avatar>
-                          <q-avatar class="q-pa-sm" size="28px">
-                            <img src="https://cdn.pixabay.com/photo/2015/09/18/11/46/man-945482__480.jpg">
-                          </q-avatar>
-                          <q-avatar class="q-pa-sm" size="28px">
-                            <img src="https://cdn.pixabay.com/photo/2015/07/15/06/42/homeless-845709__480.jpg">
-                          </q-avatar>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </q-card-section>
-              </q-card>
-            </div>
+            <br>
           </div>
         </div>
       </div>
@@ -290,6 +112,85 @@
 
 <script>
 export default {
-  name: 'PageCamera'
+  name: 'PageEvents',
+  data () {
+    return {
+      events: [
+      ]
+    }
+  },
+  methods:{
+    async getGroupsData()
+    {
+      let userId = this.$fb.auth().currentUser.uid;
+      let groups = await this.getGroupsByUserId(userId);
+
+      // get all events promises for each group
+      let firebaseGroups = []
+      groups.forEach(doc => {
+        // console.log(doc.data())
+        let groupWithEvents = doc.data()
+        groupWithEvents.events = this.getEventsByGroupId(doc.id)
+        firebaseGroups.push(groupWithEvents)
+      })
+
+      // await the event promises and save all event Data
+      let firebaseEvents = []
+      for (let groupIndex in firebaseGroups){
+        let events = await firebaseGroups[groupIndex].events
+        events.forEach(event => {
+          firebaseEvents.push(event.data())
+        })
+      }
+      for (let eventIndex in firebaseEvents){
+        let items = firebaseEvents[eventIndex].Items
+        for (let itemIndex in items)
+        {
+          items[itemIndex] = await this.getItemByItemId(items[itemIndex].id)
+          items[itemIndex].Creator = await this.getUserByUserId(items[itemIndex].Creator.id)
+          items[itemIndex].Shopper = await this.getUserByUserId(items[itemIndex].Shopper.id)
+        }
+      }
+      console.log(firebaseEvents)
+      this.events = firebaseEvents
+    },
+    getGroupsByUserId(userId){
+      let userRef = this.$firestore.collection("users").doc(userId);
+      let groups = this.$firestore.collection("Groups").where("Users", "array-contains", userRef).get();
+      return groups;
+    },
+    getEventsByGroupId(groupId){
+      let groupRef = this.$firestore.collection("Groups").doc(groupId);
+      let groupEvents = this.$firestore.collection("Events").where("Group", "==", groupRef).get();
+      return groupEvents;
+    },
+    async getItemByItemId(itemId){
+      let docRef = this.$firestore.collection("Items").doc(itemId);
+      var returnValue
+      await docRef.get().then(function(doc) {
+        if (doc.exists) {
+          returnValue = doc.data()
+        }
+      }).catch(function(error) {
+        console.log("Error getting item:", error);
+      });
+      return returnValue;
+    },
+    async getUserByUserId(userId){
+      let docRef = this.$firestore.collection("user").doc(userId);
+      var returnValue
+      await docRef.get().then(function(doc) {
+        if (doc.exists) {
+          returnValue = doc.data()
+        }
+      }).catch(function(error) {
+        console.log("Error getting user:", error);
+      });
+      return returnValue;
+    }
+  },
+  async created() {
+    await this.getGroupsData();
+  }
 }
 </script>
